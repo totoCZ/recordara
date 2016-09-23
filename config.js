5653a0057e782b4336bec7c0808a25fe9cb15c9e
Tomáš Hetmer <tom@hetmer.cz>
2016-09-23 02:04:54 +0200

    halve recording early start

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.apiKey = '';
 config.calendarId = 'ul6joarfkgroeho84vpieeaakk' // this is in your iCal, html, etc. URLs
 
 // announce before ...
-config.announceEarly = 5000; // milliseconds
+config.announceEarly = 2500; // milliseconds
 
 // calendar limits
 config.maxResults = 150; // at least 2
