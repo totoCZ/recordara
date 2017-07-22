2b21c602086e544b83d16aa4d50735a3f1f1cfa0
Tomáš Hetmer <tom@hetmer.cz>
2017-07-22 22:03:58 +0200

    sss - specific sync system, garbage version alpha

--- do_sync.sh
+++ do_sync.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 cd /opt/recordara
-aws s3 sync output s3://recordara --exclude "*.wav"
+
+#aws s3 sync output s3://recordara --exclude "*.wav"
+while read p; do
+
+if [[ $p == *"/"* ]]; then
+  f_path="$p.m4a"
+  aws s3 cp $f_path s3://recordara
+  f_path="$p.png"
+  aws s3 cp $f_path s3://recordara
+fi
+
+done </opt/recordara/synclist
+
+> /opt/recordara/synclist
+
 find /opt/recordara/output/* -mtime +7 -exec rm {} \;
